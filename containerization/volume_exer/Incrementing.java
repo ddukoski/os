4d01f8c47cab31a359d87022058cde14204e669e
David Dukoski <duko_7@outlook.com>
2024-04-27 11:28:12 +0200

    Add working version of volume testing

--- containerization/volume_exer/Incrementing.java
+++ containerization/volume_exer/Incrementing.java
@@ -1,16 +1,22 @@
 package containerization.volume_exer;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Incrementing {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =  new BufferedReader( new FileReader("increment.txt") );
-        int num = Integer.parseInt(br.readLine());
+        File make = new File(".\\datavirt\\increment.txt");
+        int num;
+        if (make.createNewFile()) {
+            num = 0;
+        } else {
+            Scanner s = new Scanner(make);
+            num = Integer.parseInt(s.nextLine()) + 1;
+        }
 
+        FileWriter fw = new FileWriter(".\\datavirt\\increment.txt");
+        fw.write(Integer.toString(num));
         System.out.println(num);
-
-        FileWriter fw = new FileWriter("increment.txt");
-        fw.write(Integer.toString(num + 1));
         fw.close();
     }
 }
