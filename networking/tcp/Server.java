cd84feb7298978d70b3ceaa3a354533a031d58f1
David Dukoski <duko_7@outlook.com>
2024-05-02 18:31:58 +0200

    Add client-server to tcp package

--- networking/tcp/Server.java
+++ networking/tcp/Server.java
@@ -1,4 +1,4 @@
-package networking;
+package networking.tcp;
 
 import java.io.IOException;
 import java.net.ServerSocket;
--- networking/tcp/Worker.java
+++ networking/tcp/Worker.java
@@ -1,4 +1,4 @@
-package networking;
+package networking.tcp;
 
 import java.io.*;
 import java.net.Socket;
