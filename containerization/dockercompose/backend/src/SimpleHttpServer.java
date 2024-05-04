7dea26a513e6a5f895b1cd3e76241956eedb266f
David Dukoski <duko_7@outlook.com>
2024-05-04 18:12:12 +0200

    Add docker compose exercise example

---
+++ containerization/dockercompose/backend/src/SimpleHttpServer.java
@@ -0,0 +1,45 @@
+package containerization.dockercompose.backend.src;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SimpleHttpServer {
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new java.net.InetSocketAddress(8080), 0);
+
+        server.createContext("/", new MyHandler());
+
+        server.setExecutor(null); // creates a default executor
+
+        server.start();
+
+        System.out.println("Server is running on port 8080...");
+
+    }
+
+
+    static class MyHandler implements HttpHandler {
+
+        @Override
+
+        public void handle(HttpExchange t) throws IOException {
+
+            String response = "Hello from Java HTTP server!";
+
+            t.sendResponseHeaders(200, response.length());
+
+            OutputStream os = t.getResponseBody();
+
+            os.write(response.getBytes());
+
+            os.close();
+
+        }
+
+    }
+}
---
+++ out/production/os/containerization/dockerfile_test/makeimage.bat
@@ -0,0 +1,2 @@
+docker build -t hello-world-test .
+docker run hello-world-test
